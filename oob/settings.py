e21e211cc759d638b100374e16d78120c6d06e3b
JokEr <trung.ng1992@gmail.com>
2018-10-23 23:50:01 +0700

    commit

--- oob/settings.py
+++ oob/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'middlewares.header_authentication.Checksum_Header'
+    #'middlewares.header_authentication.Checksum_Header'
 ]
 
 ROOT_URLCONF = 'oob.urls'

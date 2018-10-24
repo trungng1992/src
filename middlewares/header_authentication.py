d2bc73a2d330cefcea50eadc7f61d758f108c30b
trungng1992 <trung.ng1992@gmail.com>
2018-10-24 11:34:06 +0700

    commit

--- middlewares/header_authentication.py
+++ middlewares/header_authentication.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
-
+from rest_framework.response import Response
+from rest_framework.renderers import JSONRenderer
 
 from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST, HTTP_500_INTERNAL_SERVER_ERROR
 from django.utils.deprecation import MiddlewareMixin
@@ -7,11 +8,23 @@ from oob import settings
 import json, re
 
 class Checksum_Header(MiddlewareMixin):
+    def _response(self, data, status):
+        response = Response(
+            data,
+            content_type="application/json",
+            status=status
+        )
+        response.accepted_renderer = JSONRenderer()
+        response.accepted_media_type = "application/json"
+        response.renderer_context = {}
+
+        return response
+
     def process_request(self, request):
 
         url = request.get_full_path()
 
-        if re.match('^/api/v1/.*', url):
+        if not re.match('^/api/v1/.*', url):
             return
 
         USER_API = settings.USER_API
@@ -31,7 +44,7 @@ class Checksum_Header(MiddlewareMixin):
             return
 
         if abs(int(nTimestamp) - int(timestamp)) > 600:
-            return Response({
+            return self._response({
                 'response_code': HTTP_400_BAD_REQUEST,
                 'response_code': 'Please check timestamp'
             }, status=HTTP_400_BAD_REQUEST)
@@ -39,6 +52,9 @@ class Checksum_Header(MiddlewareMixin):
 
         strToken   = hashlib.sha256('_'.join([USER_API,PASSWORD_API,timeStamp]).encode('utf-8')).hexdigest()
         if strToken != checksumToken:
-            raise Http403
+            return self._response({
+                'response_code': HTTP_400_BAD_REQUEST,
+                'response_code': 'Invalid Account'
+            }, status=HTTP_400_BAD_REQUEST)
 
         return
